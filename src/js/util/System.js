ade208891917b868a39d97e5ef181c00f15d2b87
nguyenhiep2805 <77200364+nguyenhiep2805@users.noreply.github.com>
2022-05-09 20:30:51 +0700

    Update System.js

--- src/js/util/System.js
+++ src/js/util/System.js
@@ -3,7 +3,7 @@ const DEV = process.env.REACT_APP_DEV;
 const LOCAL_CLIENT = "http://localhost:3000";
 const LOCAL_SERVER = "http://192.168.1.74:3300";
 
-const DEPLOYED_HOME = "https://when4meet.vercel.app";
+const DEPLOYED_HOME = "https://meet.nguyenhiep.me";
 const DEPLOYED_SERVER = "https://avail-auth.vercel.app";
 
 export const HOME_URL = DEV ? LOCAL_CLIENT : DEPLOYED_HOME;
